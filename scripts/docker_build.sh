561a58b27cbabf55762786e5ef6288a1cd4a01ad
Bobby <karma0@gmail.com>
2016-09-09 16:13:00 -0600

    Updated scripts and theme

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t robotstudio-website .
-docker tag robotstudio-website:latest 656288215726.dkr.ecr.us-east-1.amazonaws.com/robotstudio-website:latest
-#docker push 656288215726.dkr.ecr.us-east-1.amazonaws.com/robotstudio-website:latest
+docker build -t bobbysocial-website .
+docker tag bobbysocial-website:latest 656288215726.dkr.ecr.us-east-1.amazonaws.com/bobbysocial-website:latest
+#docker push 656288215726.dkr.ecr.us-east-1.amazonaws.com/bobbysocial-website:latest
 
 exit 0
