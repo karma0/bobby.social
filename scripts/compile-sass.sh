c6450e8ff9f0fd0651aadd007274391b45cee796
Bobby <karma0@gmail.com>
2016-09-11 13:36:34 -0600

    Migrated to a static site

--- scripts/compile-sass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sass style/site.scss:static/css/site.css
-sass style/site.scss:static/css/site.min.css --style compressed
-
-exit $?
--- scripts/docker_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t bobbysocial-website .
-docker tag bobbysocial-website:latest 656288215726.dkr.ecr.us-east-1.amazonaws.com/bobbysocial-website:latest
-#docker push 656288215726.dkr.ecr.us-east-1.amazonaws.com/bobbysocial-website:latest
-
-exit 0
